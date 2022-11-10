06a2a04eadbfd63c6723dffeb590ffb5bf06d2ea
luxshd <luxshd@gmail.com>
2022-11-10 23:33:43 +0200

    upd hw6

--- Lesson 3/Homework 6.py
+++ Lesson 3/Homework 6.py
@@ -3,7 +3,7 @@
 arr = [12, 3, 4, 10]      #=> [10, 12, 3, 4]
 # arr = [1]                 #=> [1]
 # arr = []                  #=> []
-if len(arr) and len(arr) > 1:
+if len(arr) > 1:
     arr.insert(0, arr.pop())
     print(arr)
 else:
