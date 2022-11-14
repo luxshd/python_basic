4565830606ffd2ca2445f36bba534370ede56d04
luxshd <luxshd@gmail.com>
2022-11-14 23:04:38 +0200

    Init hw9

---
+++ Lesson 4/Homework 9.py
@@ -0,0 +1,11 @@
+import random
+
+arr = [random.randrange(1, 10) for i in range(random.randrange(3, 7))]
+new_arr = arr[0:3:2]
+new_arr.append(arr[-2])
+
+print('Сгенерированный список: ')
+print(arr)
+print()
+print('Обработанный список:')
+print(new_arr)
