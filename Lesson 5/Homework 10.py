f153cd4b379071848a9d11b37d0b423317c7b874
luxsh <luxshd@gmail.com>
2022-11-22 21:00:39 +0200

    Upd hw10 and hw12

--- Lesson 5/Homework 10.py
+++ Lesson 5/Homework 10.py
@@ -1,37 +1,52 @@
-allowed_chars = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u',
-    'v', 'w', 'x', 'y', 'z', '0', '1',
-    '2', '3', '4', '5', '6', '7', '8',
-    '9', '_'
-]
-kwlist = [
-    'False', 'None', 'True', 'and', 'as', 'assert',
-    'async', 'await', 'break', 'class', 'continue',
-    'def', 'del', 'elif', 'else', 'except', 'finally',
-    'for', 'from', 'global', 'if', 'import', 'in', 'is',
-    'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
-    'return', 'try', 'while', 'with', 'yield'
-]
+# allowed_chars = [
+#     'a', 'b', 'c', 'd', 'e', 'f', 'g',
+#     'h', 'i', 'j', 'k', 'l', 'm', 'n',
+#     'o', 'p', 'q', 'r', 's', 't', 'u',
+#     'v', 'w', 'x', 'y', 'z', '0', '1',
+#     '2', '3', '4', '5', '6', '7', '8',
+#     '9', '_'
+# ]
+# kwlist = [
+#     'False', 'None', 'True', 'and', 'as', 'assert',
+#     'async', 'await', 'break', 'class', 'continue',
+#     'def', 'del', 'elif', 'else', 'except', 'finally',
+#     'for', 'from', 'global', 'if', 'import', 'in', 'is',
+#     'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
+#     'return', 'try', 'while', 'with', 'yield'
+# ]
+# var_name = input('Введите имя переменной: ').strip()
+# result = 'False'
+# while True:
+#     if var_name in kwlist:
+#         break
+#     if var_name[0].isdigit():
+#         break
+#     if '__' in var_name:
+#         break
+#     for char in var_name:
+#         if char in allowed_chars:
+#             if char != '_' and char.isupper():
+#                 result = 'False'
+#                 break
+#             else:
+#                 result = 'True'
+#         else:
+#             result = 'False'
+#             break
+#     break
+# print(result)
+
+import keyword
+
+keywords = keyword.kwlist
 var_name = input('Введите имя переменной: ').strip()
-result = 'False'
-while True:
-    if var_name in kwlist:
-        break
-    if var_name[0].isdigit():
-        break
-    if '__' in var_name:
-        break
-    for char in var_name:
-        if char in allowed_chars:
-            if char != '_' and char.isupper():
-                result = 'False'
-                break
-            else:
-                result = 'True'
+result = False
+if '__' not in var_name and not var_name[0].isdigit() and var_name not in keywords:
+    for e in var_name:
+        if e == '_' or (e.isalpha() and e.islower()) or e.isdigit():
+            result = True
         else:
-            result = 'False'
+            result = False
             break
-    break
+
 print(result)
--- Lesson 5/Homework 12.py
+++ Lesson 5/Homework 12.py
@@ -1,18 +1,29 @@
+# import string
+#
+# symbols = string.punctuation
+# s = 'Should, I. subscribe? Yes!'  # -> #ShouldISubscribeYes
+# x = '#'
+# i = 0
+#
+# while i < len(s.strip()):
+#     if s[i] == "'" and s[i + 1] == 's':     # Проверка на наличие в англ. словах апострофа, без разделения на разные слова
+#         s = s.replace(s[i], '')
+#     if s[i] in symbols:                     # Если есть символ пунктуации, заменить его на пробел
+#         s = s.replace(s[i], ' ')
+#     i += 1
+# s = '#' + s.title().replace(' ', '')        # Убираю пробелы, ставлю заглавные и сокращаю строку до 140 символов
+# if len(s) > 140:
+#     s = s[:140]
+#
+# print(s)
+
 import string
 
 symbols = string.punctuation
-s = 'Should, I. subscribe? Yes!'  # -> #ShouldISubscribeYes
-x = '#'
-i = 0
+stroke = 'Should, I. subscribe? Yes!'
+for symbol in symbols:
+    if symbol in stroke:
+        stroke = stroke.replace(symbol, ' ')
+stroke = '#' + stroke.title().replace(' ', '')[:140]
 
-while i < len(s.strip()):
-    if s[i] == "'" and s[i + 1] == 's':     # Проверка на наличие в англ. словах апострофа, без разделения на разные слова
-        s = s.replace(s[i], '')
-    if s[i] in symbols:                     # Если есть символ пунктуации, заменить его на пробел
-        s = s.replace(s[i], ' ')
-    i += 1
-s = '#' + s.title().replace(' ', '')        # Убираю пробелы, ставлю заглавные и сокращаю строку до 140 символов
-if len(s) > 140:
-    s = s[:140]
-
-print(s)
+print(stroke)
