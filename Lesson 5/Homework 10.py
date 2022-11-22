17de5334834fab2dea58b8bf704bc8b4952b3453
luxsh <luxshd@gmail.com>
2022-11-22 13:22:43 +0200

    Init hw10

--- Lesson 5/Homework 10.py
+++ Lesson 5/Homework 10.py
@@ -1,19 +1,3 @@
-# Пользователь вводит строку. Ваша задача - проверить, может ли эта строка, быть именем переменной.
-# Переменная не может начинаться с цифры, состоять только из цифр,
-# не может содержать заглавные буквы и знаки пунктуации, кроме нижнего подчеркивания "_" .
-# Также, она не может быть ни одним из зарегистрированных слов. При этом имя переменной,
-# может состоять только из одного нижнего подчеркивания "_" .
-# Зарегистрированные слова можно взять из keyword.kwlist.
-# В итоге проверки, на печать выводится True, если такое имя переменной допустимо, и False - в противном случае.
-# Примеры имен переменных и результат (=> на печать выводить не нужно :))
-# _ => True
-# x => True
-# get_value => True
-# some_super_puper_value => True
-# Get_value => False
-# get_Value => False
-# getValue => False
-# 3m => False
 allowed_chars = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n',
@@ -40,22 +24,16 @@ while True:
     if '__' in var_name:
         break
     for char in var_name:
-        if char != '_' and char.isupper():
-            result = 'False'
-            break
-        if char == '_' or char.islower():
-            if char in allowed_chars:
-                result = 'True'
-            else:
+        if char in allowed_chars:
+            if char != '_' and char.isupper():
                 result = 'False'
                 break
+            else:
+                result = 'True'
+        else:
+            result = 'False'
+            break
     break
 print(result)
-# _ => True
-# x => True
-# get_value => True
-# some_super_puper_value => True
-# Get_value => False
-# get_Value => False
-# getValue => False
-# 3m => False
+фывф = "фыв"
+print(фывф)
