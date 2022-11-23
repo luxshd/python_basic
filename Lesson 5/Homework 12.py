a802680bae8fedac67914a3e3b767910a560331b
luxsh <luxsh@gmail.com>
2022-11-23 13:31:41 +0200

    upd

--- Lesson 5/Homework 12.py
+++ Lesson 5/Homework 12.py
@@ -1,22 +1,3 @@
-# import string
-#
-# symbols = string.punctuation
-# s = 'Should, I. subscribe? Yes!'  # -> #ShouldISubscribeYes
-# x = '#'
-# i = 0
-#
-# while i < len(s.strip()):
-#     if s[i] == "'" and s[i + 1] == 's':     # Проверка на наличие в англ. словах апострофа, без разделения на разные слова
-#         s = s.replace(s[i], '')
-#     if s[i] in symbols:                     # Если есть символ пунктуации, заменить его на пробел
-#         s = s.replace(s[i], ' ')
-#     i += 1
-# s = '#' + s.title().replace(' ', '')        # Убираю пробелы, ставлю заглавные и сокращаю строку до 140 символов
-# if len(s) > 140:
-#     s = s[:140]
-#
-# print(s)
-
 import string
 
 symbols = string.punctuation
