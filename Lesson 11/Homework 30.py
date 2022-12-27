d01a35c736d94e5eb07cdffb7a097403c5750748
luxsh <luxshd@gmail.com>
2022-12-27 15:34:55 +0200

    Upd HW30

--- Lesson 11/Homework 30.py
+++ Lesson 11/Homework 30.py
@@ -1,6 +1,6 @@
 def is_even(num: int) -> bool:
     """определяет, чётное ли число"""
-    return True if str(num)[-1] in ['0', '2', '4', '6', '8'] else False
+    return str(num)[-1] in ['0', '2', '4', '6', '8']
 
 
 print(is_even(2494563894038 ** 2))
